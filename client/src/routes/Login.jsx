9197fa2edc579bbd64f93774d29beceb7bbac911
nc1z <nc1zdev@gmail.com>
2022-12-10 21:58:43 +0800

    [Login, Signup] updated useNavigate on success

--- client/src/routes/Login.jsx
+++ client/src/routes/Login.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -12,6 +13,8 @@ const Login = () => {
 
     setEmail("");
     setPassword("");
+    // On Success
+    navigate("/home");
   };
 
   return (
--- client/src/routes/Signup.jsx
+++ client/src/routes/Signup.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -12,6 +13,8 @@ const Signup = () => {
 
     setEmail("");
     setPassword("");
+    // On Success
+    navigate("/home");
   };
 
   return (

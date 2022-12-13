fec56c4651bdd11dd9d75101615d35d7758710ea
Desmond Tan <desmondtansl90@gmail.com>
2022-12-14 01:34:39 +0800

    did validation checks on email and password

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,9 +1,25 @@
 import express from "express";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/signup", async (req,res) => {
+router.post(
+"/signup", 
+body("email").isEmail().withMessage("Invalid email"),
+body("password").isLength({min: 8}).withMessage("Password is too short"),
+async (req,res) => {
+    const validationErrors = validationResult(req);
+    if (!validationErrors.isEmpty()) {
+        const errors = validationErrors.array().map(error => {
+            return {
+                msg: error.msg
+            }
+        });
+        return res.json({errors})
+    };
+
     const { email, password } = req.body;
+
     res.json({
         email,
         password

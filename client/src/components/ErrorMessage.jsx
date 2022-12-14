db0d3184b465249f8a4eab6508f38d0bcfed7eb6
nc1z <nc1zdev@gmail.com>
2022-12-14 22:19:26 +0800

    [ErrorMessage] refactor code

--- client/src/components/ErrorMessage.jsx
+++ client/src/components/ErrorMessage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ErrorMessage = ({ error }) => {
   return (
-    <div className="max-w-max mx-auto mt-3 text-white bg-red-500/80 font-medium">
+    <div className="max-w-max mx-auto mt-3 text-white uppercase bg-red-500/80 font-medium">
       {error}
     </div>
   );

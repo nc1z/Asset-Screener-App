9653b4ee3cd43f6582a45b4199d8fca4a6ce9aa7
Desmond Tan <desmondtansl90@gmail.com>
2022-12-15 01:18:19 +0800

    hashed pw and saved new test user to DB

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { body, validationResult } from "express-validator";
 import User from "../models/User.js";
+import bcrypt from "bcryptjs";
 
 const router = express.Router();
 
@@ -33,6 +34,13 @@ async (req,res) => {
             data:"",
         })
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await User.create({
+        email,
+        password: hashedPassword
+    });
 
     res.json(user);
 });

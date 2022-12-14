1fb7f8349e87bbe8051546ba8d8cfa9578e27fc2
nc1z <nc1zdev@gmail.com>
2022-12-14 22:17:00 +0800

    [ErrorMessage] Created error message component

---
+++ client/src/components/ErrorMessage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ErrorMessage = ({ error }) => {
+  return (
+    <div className="max-w-max mx-auto mt-3 text-white bg-red-500/80 font-medium">
+      {error}
+    </div>
+  );
+};
+
+export default ErrorMessage;

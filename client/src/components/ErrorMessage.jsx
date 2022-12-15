b7106a485340ecdd86fa8ea148581827c841b9de
nc1z <nc1zdev@gmail.com>
2022-12-15 21:35:04 +0800

    [ErrorMessage] Added check for Error Array

--- client/src/components/ErrorMessage.jsx
+++ client/src/components/ErrorMessage.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 
 const ErrorMessage = ({ error }) => {
+  if (Array.isArray(error)) {
+    return (
+      <>
+        {error.map((err, idx) => (
+          <div
+            key={idx}
+            className="max-w-max mx-auto mt-3 text-white uppercase bg-red-500/80 font-medium"
+          >
+            {err.msg}
+          </div>
+        ))}
+      </>
+    );
+  }
   return (
     <div className="max-w-max mx-auto mt-3 text-white uppercase bg-red-500/80 font-medium">
       {error}

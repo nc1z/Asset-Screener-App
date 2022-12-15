c6dec7064ca07503184237eb1e18692235fd839d
Desmond Tan <desmondtansl90@gmail.com>
2022-12-15 14:24:25 +0800

    watchlist schema done

---
+++ server/src/models/watchlist.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const watchlistSchema = new Schema(
+  {
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      required: true,
+      unique: true
+    },
+    items: [
+        {
+            symbol: String,
+            coin: String,
+            image: String,
+        }
+    ]
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Watchlist", watchlistSchema);

f408d1bb30191f1830622665a956c9f0ae4e1cf5
nc1z <nc1zdev@gmail.com>
2022-12-18 11:55:36 +0800

    [Portfolio] Updated link to Transaction history

--- client/src/routes/Portfolio.jsx
+++ client/src/routes/Portfolio.jsx
@@ -6,7 +6,10 @@ const Portfolio = () => {
   return (
     <div className="mt-36">
       <div>Total Portfolio Value here + line or pie chart</div>
-      <Link to="/portfolio/ticket">Create Ticket</Link>
+      <div className="flex gap-4">
+        <Link to="/portfolio/ticket">Create Ticket</Link>
+        <Link to="/portfolio/history">Transaction History</Link>
+      </div>
       <PortfolioTable />
     </div>
   );
